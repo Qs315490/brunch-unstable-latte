100fda3bc4a80fa8f72f30c79625cc2c90e480d4
Qs315490 <qs315490@qq.com>
2025-05-08 14:27:23 +0800

    细节修正

--- build_brunch.sh
+++ build_brunch.sh
@@ -17,12 +17,12 @@ kernel_version="$(file ./out/arch/x86/boot/bzImage | cut -d' ' -f9)"
 cp ./out/arch/x86/boot/bzImage $root_dir/tmp/$kernel/vmlinuz-"$kernel_version" || { echo "Failed to copy the kernel $kernel"; exit 1; }
 make -j"$NTHREADS" O=out INSTALL_MOD_STRIP=1 INSTALL_MOD_PATH=$root_dir/tmp/$kernel modules_install || { echo "Failed to install modules for kernel $kernel"; exit 1; }
 
-mkdir -p $root_dir/tmp/$kernel/kernel/usr/src/linux-headers-"$kernel_version" || { echo "Failed to create the linux-headers directory for kernel $kernel"; exit 1; }
-cp -r ./headers $root_dir/tmp/$kernel/usr/src/linux-headers-"$kernel_version" || { echo "Failed to replace the build directory for kernel $kernel"; exit 1; }
+mkdir -p $root_dir/tmp/$kernel/usr/src/linux-headers-"$kernel_version" || { echo "Failed to create the linux-headers directory for kernel $kernel"; exit 1; }
+cp -r ./headers/* $root_dir/tmp/$kernel/usr/src/linux-headers-"$kernel_version" || { echo "Failed to replace the build directory for kernel $kernel"; exit 1; }
 
 cd $root_dir/tmp/$kernel || { echo "Failed to enter directory for kernel $kernel"; exit 1; }
 tar zcf $root_dir/out/kernel-"$kernel_version".tar.gz * --owner=0 --group=0 || { echo "Failed to create archive for kernel $kernel"; exit 1; }
 cd $root_dir || { echo "Failed to cleanup for kernel $kernel"; exit 1; }
-rm -rf ./tmp/$kernel || { echo "Failed to cleanup for kernel $kernel"; exit 1; }
+rm -rf ./tmp || { echo "Failed to cleanup for kernel $kernel"; exit 1; }
 
 done
